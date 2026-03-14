cc1b29f14617fbf3fb90fd1e5428200d495f4883
shivii15 <shivangi.nigam@bennett.edu.in>
2026-03-14 17:38:49 +0530

    change data_loader

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, RobustScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler, RobustScaler
 import torch
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 
 def preprocess_iot_data(file_path):
     df = pd.read_csv(file_path)
