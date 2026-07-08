451b503fbc4cd51fc78f2e4d93076a48f4bb1f86
shivii15 <shivangi.nigam@bennett.edu.in>
2026-07-08 12:17:56 +0530

    Phase 4A: working on train anomaly detector

--- train_anomaly_detector.py
+++ train_anomaly_detector.py
@@ -5,7 +5,7 @@ import os
 import argparse
 from datetime import datetime
 from sklearn.ensemble import IsolationForest
-from data.data_loader import preprocess_iot_data
+from data.data_loader import load_dataset
 
 def train_anomaly_module(args):
     timestamp = datetime.now().strftime("%Y%m%d")
@@ -13,7 +13,16 @@ def train_anomaly_module(args):
     
     # 1. Load data using your specialized loader
     # We capture 'scaler' to ensure the detector uses the same normalization as teachers
-    X, y, le, scaler = preprocess_iot_data(args.data_path, num_parts=args.num_parts)
+    X, y, le, scaler = load_dataset(args)
+
+    print(f"\nDataset : {args.dataset}")
+
+    print("\nDataset Summary")
+    print("-" * 40)
+    print(f"Samples      : {len(X):,}")
+    print(f"Features     : {X.shape[1]}")
+    print(f"Classes      : {len(le.classes_)}")
+    print("-" * 40)
     
     # 2. Robust Label Detection (Handles 'Benign' variants)
     benign_idx = None
@@ -74,12 +83,29 @@ def train_anomaly_module(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="SOTA Anomaly Module for Multi-Teacher TGKD")
-    
-    parser.add_argument('--data_path', type=str, required=True)
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="cic",
+        choices=["cic", "nbaiot"],
+        help="Dataset to use"
+    )
+    #parser.add_argument('--data_path', type=str, required=True)
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        default=None,
+        help="Dataset directory (optional for N-BaIoT)"
+    )
     parser.add_argument('--save_path', type=str, default="models/iso_forest_iot_resnet_24march.pkl")
     parser.add_argument('--num_parts', type=int, default=10)
     parser.add_argument('--n_estimators', type=int, default=100)
     parser.add_argument('--contamination', type=float, default=0.01)
 
     args = parser.parse_args()
+
+    if args.dataset == "cic" and args.data_path is None:
+        raise ValueError(
+            "--data_path is required when using the CIC-IoT dataset."
+        )
     train_anomaly_module(args)
